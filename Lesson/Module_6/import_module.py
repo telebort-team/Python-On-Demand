49c07c0105e181b2bfc57aaa6a861f59c9287fed
OoiBP <bpooi0702@gmail.com>
2019-09-01 16:07:41 +0800

    Add Lesson Module 6

---
+++ Lesson/Module_6/import_module.py
@@ -0,0 +1,4 @@
+import math
+a = 144
+b = math.sqrt(a)
+print(b)
---
+++ Lesson/Module_6/random_integer.py
@@ -0,0 +1,4 @@
+from random import randint
+x = ['Apple', 'Orange', 'Grape', 'Durian', 'Pineapple', 'Kiwi']
+ind = randint(2, 5)		# Generate random number between 2 and 5
+print(x[ind])
