df2839181439d54255fa07b9ca919678f2e62e3a
OoiBP <bpooi0702@gmail.com>
2019-09-01 14:32:14 +0800

    Add Lesson Module 2

---
+++ Lesson/Module_2/main.py
@@ -0,0 +1,30 @@
+# Integer
+x = 10
+y = 20
+x + y
+y = 3
+x % y
+
+# Float
+p = 3.142
+r = 5.0
+2 * p * r
+
+# String
+s = "hello, world!"
+print(s)
+s.upper()
+s.capitalize()
+s.find('or')
+s.isdigit()
+s.isalpha()
+s.isalnum()
+
+# Boolean
+state1 = True
+state2 = False
+state1 and state2
+state1 or state2
+not state1
+5 > 2
+5 < 2
