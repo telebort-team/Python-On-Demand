40f4ed2e828058edb3916c48d3f8eeae8bff7a44
OoiBP <bpooi0702@gmail.com>
2019-09-01 16:03:54 +0800

    Add Lesson Module 5

---
+++ Lesson/Module_5/define_function.py
@@ -0,0 +1,7 @@
+def say_hello(name):
+    print("Hello " + name)
+
+
+say_hello("Alex")
+print("Hi")
+say_hello("Brenda")
---
+++ Lesson/Module_5/function_with_return.py
@@ -0,0 +1,6 @@
+def add_number(x, y):
+    return x + y
+
+
+answer = add_number(20, 45)
+print(answer)
